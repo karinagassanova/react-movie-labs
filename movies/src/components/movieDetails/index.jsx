7c13eec75c026de6844602bb005fcbe124bfb65c
karinegassanova <20102374@mail.wit.ie>
2025-10-08 13:47:42 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
@@ -7,20 +6,23 @@ import StarRate from "@mui/icons-material/StarRate";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
-
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews";
 
 const root = {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: 1.5,
-    margin: 0,
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  listStyle: "none",
+  padding: 1.5,
+  margin: 0,
 };
+
 const chip = { margin: 0.5 };
 
-const MovieDetails = ( props) => {
-  const movie = props.movie
+const MovieDetails = ({ movie }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
@@ -32,24 +34,22 @@ const MovieDetails = ( props) => {
         {movie.overview}
       </Typography>
 
-      <Paper 
-        component="ul" 
-        sx={{...root}}
-      >
+      <Paper component="ul" sx={{ ...root }}>
         <li>
-          <Chip label="Genres" sx={{...chip}} color="primary" />
+          <Chip label="Genres" sx={{ ...chip }} color="primary" />
         </li>
         {movie.genres.map((g) => (
           <li key={g.name}>
-            <Chip label={g.name} sx={{...chip}} />
+            <Chip label={g.name} sx={{ ...chip }} />
           </li>
         ))}
       </Paper>
-      <Paper component="ul" sx={{...root}}>
+
+      <Paper component="ul" sx={{ ...root }}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
           icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
+          label={`$${movie.revenue?.toLocaleString() || "N/A"}`}
         />
         <Chip
           icon={<StarRate />}
@@ -57,19 +57,30 @@ const MovieDetails = ( props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+
       <Fab
         color="secondary"
         variant="extended"
+        onClick={() => setDrawerOpen(true)}
         sx={{
-            position: "fixed",
-            bottom: 2,
-            right: 2
+          position: "fixed",
+          bottom: "1em",
+          right: "1em",
         }}
       >
-        <NavigationIcon />
+        <NavigationIcon sx={{ mr: 1 }} />
         Reviews
       </Fab>
-      </>
+
+      <Drawer
+        anchor="top"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <MovieReviews movie={movie} />
+      </Drawer>
+    </>
   );
 };
-export default MovieDetails ;
+
+export default MovieDetails;
