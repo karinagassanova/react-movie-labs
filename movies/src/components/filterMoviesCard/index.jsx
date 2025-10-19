c4ebdd30d04fb6919b5fe4ac16f9946f7e3ed715
karinegassanova <20102374@mail.wit.ie>
2025-10-19 12:50:29 +0100

    Removed extra text to filter movies

--- movies/src/components/filterMoviesCard/index.jsx
+++ movies/src/components/filterMoviesCard/index.jsx
@@ -127,13 +127,6 @@ export default function FilterMoviesCard(props) {
         image={img}
         title="Filter"
       />
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-          <br />
-        </Typography>
-      </CardContent>
     </Card>
   );
 }
