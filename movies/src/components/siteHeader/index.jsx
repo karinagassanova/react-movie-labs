f4ee3209a8584a8d0f93d32fc5e6be5381e429da
karinegassanova <20102374@mail.wit.ie>
2025-10-19 17:04:09 +0100

    Changed the site header and added an image as TMDB logo to match the website

--- movies/src/components/siteHeader/index.jsx
+++ movies/src/components/siteHeader/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -11,6 +10,7 @@ import { useNavigate } from "react-router";
 import { styled } from '@mui/material/styles';
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import tmdbLogo from '../../images/tmdb-logo.png';
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
@@ -41,14 +41,21 @@ const SiteHeader = () => {
 
   return (
     <>
-      <AppBar position="fixed" color="secondary">
-        <Toolbar>
-          <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            TMDB Client
-          </Typography>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            All you ever wanted to know about Movies!
-          </Typography>
+     <AppBar
+  position="fixed"
+  sx={{
+    background: 'linear-gradient(90deg, #01b4e4 0%, #90cea1 100%)',
+    boxShadow: '0px 2px 8px rgba(0,0,0,0.2)',
+  }}
+>
+      <Toolbar>
+      <img
+  src={tmdbLogo}
+  alt="TMDB Logo"
+  style={{ height: 60,width:100, cursor: 'pointer' }}
+  onClick={() => navigate("/")}
+/>
+
             {isMobile ? (
               <>
                 <IconButton
@@ -91,6 +98,14 @@ const SiteHeader = () => {
                   <Button
                     key={opt.label}
                     color="inherit"
+                    sx={{
+                      fontWeight: 'bold',
+                      '&:hover': {
+                        backgroundColor: 'rgba(255,255,255,0.2)',
+                        borderRadius: 1
+                      },
+                      mx: 1
+                    }}
                     onClick={() => handleMenuSelect(opt.path)}
                   >
                     {opt.label}
