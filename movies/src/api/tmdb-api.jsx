b1914bfb8d1e48e7b215124bc3dfaeb03fb38ee6
karinegassanova <20102374@mail.wit.ie>
2025-10-26 14:45:40 +0000

    Implemented pagination for now playing movies page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -138,9 +138,9 @@ export const getMovie = (args) => {
   };
   
   // https://developer.themoviedb.org/reference/movie-now-playing-list
-  export const getNowPlayingMovies = async () => {
+  export const getNowPlayingMovies = async (page = 1) => {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=${page}`
     );
     if (!response.ok) {
       const error = await response.json();
--- movies/src/pages/nowPlayingMoviesPage.jsx
+++ movies/src/pages/nowPlayingMoviesPage.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getNowPlayingMovies } from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
 import Spinner from "../components/spinner";
 import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const NowPlayingMoviesPage = () => {
+  const [page, setPage] = useState(1);
   const { data, error, isLoading, isError } = useQuery({
-    queryKey: ["nowPlayingMovies"],
-    queryFn: getNowPlayingMovies,
+    queryKey: ["nowPlayingMovies",page],
+    queryFn: () =>  getNowPlayingMovies(page),
   });
 
   if (isLoading) return <Spinner />;
@@ -18,11 +21,34 @@ const NowPlayingMoviesPage = () => {
   const movies = data.results;
 
   return (
+    <>
     <PageTemplate
       title="Now Playing"
       movies={movies}
       action={(movie) => <AddToFavoritesIcon movie={movie} />}
     />
+     <Stack spacing={2} sx={{ alignItems: 'center', mt: 2 }}>
+  <Pagination
+    count={data?.total_pages > 500 ? 500 : data?.total_pages} // TMDB limits pages to 500
+    page={page}
+    onChange={(event, value) => setPage(value)}
+    sx={{
+      "& .MuiPaginationItem-root": {
+        color: "#032541",
+        borderColor: "#032541",
+      },
+      "& .MuiPaginationItem-root:hover": {
+        backgroundColor: "#032541",
+        color: "#fff",
+      },
+      "& .MuiPaginationItem-root.Mui-selected": {
+        backgroundColor: "#032541",
+        color: "#fff",
+      },
+    }}
+  />
+</Stack>
+      </>
   );
 };
 
