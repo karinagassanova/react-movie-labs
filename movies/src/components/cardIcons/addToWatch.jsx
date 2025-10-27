a9c2cc33472f8023c2b780711c9a0ad7b5f65e9d
karinegassanova <20102374@mail.wit.ie>
2025-10-27 14:15:49 +0000

    Added a new MUI component - popover to add to must watch list.

--- movies/src/components/cardIcons/addToWatch.jsx
+++ movies/src/components/cardIcons/addToWatch.jsx
@@ -1,16 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import { MoviesContext } from "../../contexts/moviesContext";
+import Typography from "@mui/material/Typography";
+import Popover from "@mui/material/Popover";
 
 const AddToWatchIcon = ({ movie }) => {
   const { addToMustWatch } = useContext(MoviesContext);
-
-  const handleClick = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
     addToMustWatch(movie);
+    setAnchorEl(event.currentTarget);
   };
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+// Popover Reference https://mui.com/material-ui/react-popover/
+  const open = Boolean(anchorEl);
+  const id = open ? "watch-popover" : undefined;
+
   return (
+    <>
     <IconButton aria-label="add to watch list" onClick={handleClick}>
       <PlaylistAddIcon fontSize="large"
         sx={{
@@ -19,7 +30,26 @@ const AddToWatchIcon = ({ movie }) => {
         }}
       />
     </IconButton>
-  );
+  <Popover
+  id={id}
+  open={open}
+  anchorEl={anchorEl}
+  onClose={handleClose}
+  anchorOrigin={{
+    vertical: "bottom",
+    horizontal: "left",
+  }}
+  transformOrigin={{
+    vertical: "top",
+    horizontal: "left",
+  }}
+>
+  <Typography sx={{ p: 2 }}>
+    Added "{movie.title}" to Must Watch list!
+  </Typography>
+</Popover>
+</>
+);
 };
 
 export default AddToWatchIcon;
